bcfaa5ac93d615ad9fb39e85d07770214f8a0edd
ganangadjie <ganangaji221@gmail.com>
2023-03-28 05:36:06 +0700

    Membuat Baris dan Kolom

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 // import 'hello_world.dart';
 // import 'column_widget.dart';
-import 'row_widget.dart';
+// import 'row_widget.dart';
+import 'baris_kolom.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,8 @@ class MyApp extends StatelessWidget {
       title: 'Klinik',
       // home: HelloWorld(),
       // home: ColumnWidget(),
-      home: RowWidget(),
+      // home: RowWidget(),
+      home: BarisKolomWidget(),
     );
   }
 }
