23406145ef6b55203e1dd94e182fbeebec6de5f2
ganangadjie <ganangaji221@gmail.com>
2023-03-28 16:38:03 +0700

    Membuat Poli

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 // import 'hello_world.dart';
 // import 'column_widget.dart';
 // import 'row_widget.dart';
-import 'baris_kolom.dart';
+// import 'baris_kolom.dart';
+import '/ui/poli_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,11 +15,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Klinik',
+      title: 'Klinik APP',
       // home: HelloWorld(),
       // home: ColumnWidget(),
       // home: RowWidget(),
-      home: BarisKolomWidget(),
+      // home: BarisKolomWidget(),
+      debugShowCheckedModeBanner: false,
+      home: PoliPage(),
     );
   }
 }
