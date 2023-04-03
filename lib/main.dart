e7f66c1b8b111dde5a288661677607e479095355
ganangadjie <ganangaji221@gmail.com>
2023-04-04 06:28:59 +0700

    Membuat halaman home

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 // import 'column_widget.dart';
 // import 'row_widget.dart';
 // import 'baris_kolom.dart';
-import '/ui/poli_page.dart';
+// import '/ui/poli_page.dart';
+import 'ui/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,8 +21,7 @@ class MyApp extends StatelessWidget {
       // home: ColumnWidget(),
       // home: RowWidget(),
       // home: BarisKolomWidget(),
-      debugShowCheckedModeBanner: false,
-      home: PoliPage(),
+      home: HomePage(),
     );
   }
 }
