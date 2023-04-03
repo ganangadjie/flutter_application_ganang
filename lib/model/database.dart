63a2cee20d4a5e6b3783e8fa416046d2995d08bf
ganangadjie <ganangaji221@gmail.com>
2023-04-04 06:39:11 +0700

    Membuat Database

---
+++ lib/model/database.dart
@@ -0,0 +1,64 @@
+List<Map> pegawai = [
+  {
+    'id': 1,
+    'nip': '5453424',
+    'nama': 'Sri Setyani',
+    'tanggalLahir': '15 Mei 1997',
+    'nomorTelepon': '081898765543',
+    'email': 'sri@gmail.com',
+    'password': '123',
+  },
+  {
+    'id': 2,
+    'nip': '6522575',
+    'nama': 'Kris Fernando',
+    'tanggalLahir': '9 September 1989',
+    'nomorTelepon': '0856432198709',
+    'email': 'kris@gmail.com',
+    'password': '123',
+  },
+  {
+    'id': 3,
+    'nip': '729465',
+    'nama': 'Ulfa Nadafa',
+    'tanggalLahir': '9 Juni 1995',
+    'nomorTelepon': '082276854490',
+    'email': 'ulfa@gmail.com',
+    'password': '123',
+  },
+];
+
+List<Map> pasien = [
+  {
+    'id': 1,
+    'noRm': '8799',
+    'nama': 'Slamet Kopling',
+    'tanggalLahir': '10 Oktober 1985',
+    'nomorTelepon': '081246758876',
+    'alamat': 'Klaten',
+  },
+  {
+    'id': 2,
+    'noRm': '9654',
+    'nama': 'Agus Kotak',
+    'tanggalLahir': '08 November 1999',
+    'nomorTelepon': '087786554632',
+    'alamat': 'Surakarta',
+  },
+  {
+    'id': 3,
+    'noRm': '1093',
+    'nama': 'Mamat Karbu',
+    'tanggalLahir': '16 Maret 2000',
+    'nomorTelepon': '085209876543',
+    'alamat': 'Sukoharjo',
+  },
+  {
+    'id': 4,
+    'noRm': '2077',
+    'nama': 'Dita Leni Rafia',
+    'tanggalLahir': '14 Agustus 2001',
+    'nomorTelepon': '085712345678',
+    'alamat': 'Wonogiri',
+  },
+];
